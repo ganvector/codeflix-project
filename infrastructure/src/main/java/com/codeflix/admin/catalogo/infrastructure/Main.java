c8c3f78d78e0643c6729e9c4081fc09c86be82e3
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-12 18:08:06 -0300

    fix: wrong call to dummy use case

--- infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/Main.java
@@ -6,6 +6,5 @@ import com.codeflix.admin.catalogo.application.UseCase;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(new UseCase().execute());
     }
 }
