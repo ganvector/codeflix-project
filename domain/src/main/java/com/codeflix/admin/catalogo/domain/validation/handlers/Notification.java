6b99d3f5ef15b5c37aaf7db3bb7ccea45247917b
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-12 12:11:38 -0300

    refactor: add class that will deal with erros

---
+++ domain/src/main/java/com/codeflix/admin/catalogo/domain/validation/handlers/Notification.java
@@ -0,0 +1,54 @@
+package com.codeflix.admin.catalogo.domain.validation.handlers;
+
+import com.codeflix.admin.catalogo.domain.exceptions.DomainException;
+import com.codeflix.admin.catalogo.domain.validation.Error;
+import com.codeflix.admin.catalogo.domain.validation.ValidationHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification implements ValidationHandler {
+
+    private final List<Error> errors;
+
+    private Notification(final List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public static Notification create() {
+        return new Notification(new ArrayList<>());
+    }
+
+    public static Notification create(final Error anError) {
+        return new Notification(List.of(anError));
+    }
+
+    @Override
+    public ValidationHandler append(final Error anError) {
+        this.errors.add(anError);
+        return this;
+    }
+
+    @Override
+    public ValidationHandler append(final ValidationHandler anHandler) {
+        this.errors.addAll(anHandler.getErrors());
+        return this;
+    }
+
+    @Override
+    public ValidationHandler validate(Validation aValidation) {
+        try {
+            aValidation.validate();
+        } catch (DomainException ex) {
+            this.errors.addAll(ex.getErrors());
+        } catch (Throwable t) {
+            this.errors.add(new Error(t.getMessage()));
+        }
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return this.errors;
+    }
+}
