e53ad4d1cc25180f4520c05118fe9fae35701a34
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-11 23:13:18 -0300

    refactor: changing name size magic numbers into constants

--- domain/src/main/java/com/codeflix/admin/catalogo/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/codeflix/admin/catalogo/domain/category/CategoryValidator.java
@@ -6,6 +6,8 @@ import com.codeflix.admin.catalogo.domain.validation.Validator;
 
 public class CategoryValidator extends Validator {
 
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
     private final Category category;
 
     public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
@@ -29,7 +31,7 @@ public class CategoryValidator extends Validator {
             return;
         }
         var nameSize = name.trim().length();
-        if (nameSize > 255 || nameSize < 3) {
+        if (nameSize > NAME_MAX_LENGTH || nameSize < NAME_MIN_LENGTH) {
             this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
             return;
         }
