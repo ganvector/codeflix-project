50def60253881de9cc46afe75df3f77511377c86
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-13 12:17:33 -0300

    feat: add default profile as development

--- infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/Main.java
@@ -4,11 +4,13 @@ package com.codeflix.admin.catalogo.infrastructure;
 import com.codeflix.admin.catalogo.infrastructure.config.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
