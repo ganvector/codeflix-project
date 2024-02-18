a85d565d98eb460334e9cf859808fa14548033b7
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-18 15:30:52 -0300

    chore: add validation when create a new database entity before saving it

---
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/category/MySQLGatewayTest.java
@@ -0,0 +1,21 @@
+package com.codeflix.admin.catalogo.infrastructure.category;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@DataJpaTest
+@ComponentScan(includeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*MySQLGateway")
+})
+public @interface MySQLGatewayTest {
+}
---
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.codeflix.admin.catalogo.infrastructure.category.persistence;
+
+import com.codeflix.admin.catalogo.domain.category.Category;
+import com.codeflix.admin.catalogo.infrastructure.category.MySQLGatewayTest;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.management.DescriptorKey;
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void shouldReturnAnErrorWhenGivenAnInvalidName() {
+        final var expectedPropertyName = "name";
+        final var expectedMessage = "not-null property references a null or transient value : com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.name";
+
+        final var aCategory = Category.createCategory("Movies", "Universe's best movies", true);
+
+        final var anCategoryEntity = CategoryJPAEntity.create(aCategory);
+        anCategoryEntity.setName(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anCategoryEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+
+    @Test
+    public void shouldReturnAnErrorWhenGivenAnInvalidCreatedAt() {
+        final var expectedPropertyName = "createdAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.createdAt";
+
+        final var aCategory = Category.createCategory("Movies", "Universe's best movies", true);
+
+        final var anCategoryEntity = CategoryJPAEntity.create(aCategory);
+        anCategoryEntity.setCreatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anCategoryEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+
+    @Test
+    public void shouldReturnAnErrorWhenGivenAnInvalidUpdatedAt() {
+        final var expectedPropertyName = "updatedAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.updatedAt";
+
+        final var aCategory = Category.createCategory("Movies", "Universe's best movies", true);
+
+        final var anCategoryEntity = CategoryJPAEntity.create(aCategory);
+        anCategoryEntity.setUpdatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anCategoryEntity));
+
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualCause.getMessage());
+    }
+}
