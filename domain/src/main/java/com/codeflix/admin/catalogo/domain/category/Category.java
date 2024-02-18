f3ac2bc6a6ce3a774a462784e3e09f2bbd122c5f
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-18 11:06:15 -0300

    feat: add entity that translates domain entity to database entity and the opposite

--- domain/src/main/java/com/codeflix/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/codeflix/admin/catalogo/domain/category/Category.java
@@ -43,6 +43,26 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
+    public static Category loadCategory(
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                aName,
+                aDescription,
+                isActive,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
+    }
+
     public String getName() {
         return name;
     }
