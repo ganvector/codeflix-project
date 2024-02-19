246049cda6bdf07c3195b811774f19bc95425022
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-19 09:53:42 -0300

    refactor: change files to upper package

--- infrastructure/src/test/java/com/codeflix/admin/catalogo/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/MySQLGatewayTest.java
@@ -1,4 +1,4 @@
-package com.codeflix.admin.catalogo.infrastructure.category;
+package com.codeflix.admin.catalogo;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
--- infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/category/MainTest.java
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/category/MainTest.java
@@ -1,12 +1,12 @@
-package com.codeflix.admin.catalogo.infrastructure;
+package com.codeflix.admin.catalogo.infrastructure.category;
 
+import com.codeflix.admin.catalogo.infrastructure.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
-    @Test
     public void testMain() {
         System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
