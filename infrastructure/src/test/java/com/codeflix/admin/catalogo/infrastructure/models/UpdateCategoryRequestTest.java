68658e9a1588a753f6b0b0e72d534af075c4de61
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-20 16:25:26 -0300

    test: serializing class UpdateCategoryRequest

---
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,40 @@
+package com.codeflix.admin.catalogo.infrastructure.models;
+
+import com.codeflix.admin.catalogo.JacksonTest;
+import com.codeflix.admin.catalogo.infrastructure.category.models.CategoryResponse;
+import com.codeflix.admin.catalogo.infrastructure.category.models.UpdateCategoryRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Universe's best movies";
+        final var expectedIsActive = true;
+
+        final var json = """
+        {
+            "name": "%s",
+            "description": "%s",
+            "is_active": "%s"
+        }
+        """.formatted(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
