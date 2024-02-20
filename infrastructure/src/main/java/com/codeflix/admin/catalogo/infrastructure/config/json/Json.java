74cc8054df4eda9ac602c760c499f9c473451d42
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-20 15:50:08 -0300

    feat: implement object mapper to returns from api in a common pattern

---
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/config/json/Json.java
@@ -0,0 +1,52 @@
+package com.codeflix.admin.catalogo.infrastructure.config.json;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.concurrent.Callable;
+
+public enum Json {
+    INSTANCE;
+
+    public static ObjectMapper getMapper() {
+        return INSTANCE.mapper.copy();
+    }
+
+    public static String writeValueAsString(final Object obj) {
+        return invoke(() -> INSTANCE.mapper.writeValueAsString(obj));
+    }
+
+    public static <T> T readValue(final String json, final Class<T> tClass) {
+        return invoke(() -> INSTANCE.mapper.readValue(json, tClass));
+    }
+
+    private final ObjectMapper mapper = new Jackson2ObjectMapperBuilder()
+            .dateFormat(new StdDateFormat())
+            .featuresToDisable(
+                    DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                    DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES,
+                    DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES,
+                    SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
+            )
+            .modules(new JavaTimeModule(), new Jdk8Module(), afterburnerModule())
+            .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+            .build();
+
+    private AfterburnerModule afterburnerModule() {
+        var module = new AfterburnerModule();
+        module.setUseValueClassLoader(false);
+        return module;
+    }
+
+    private static <T> T invoke(final Callable<T> callable){
+        try {
+            return callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
