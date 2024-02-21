a24a0e5500ba52239fc0931c04a3f02a5dcfae9b
Vitor Nascimento <vitor.gan21@gmail.com>
2024-02-21 17:06:15 -0300

    feat: add genre gateway specifications

---
+++ domain/src/main/java/com/codeflix/admin/catalogo/domain/genre/GenreGateway.java
@@ -0,0 +1,20 @@
+package com.codeflix.admin.catalogo.domain.genre;
+
+import com.codeflix.admin.catalogo.domain.pagination.Pagination;
+import com.codeflix.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.Optional;
+
+public interface GenreGateway {
+
+    Genre create(Genre aGenre);
+
+    void deleteById(GenreID anId);
+
+    Optional<Genre> findById(GenreID anId);
+
+    Genre update(Genre aGenre);
+
+    Pagination<Genre> findAll(SearchQuery aQuery);
+}
+
