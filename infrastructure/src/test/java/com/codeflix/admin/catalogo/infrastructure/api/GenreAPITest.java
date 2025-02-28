96ac9dd40f9ea0a655a578c2210954ab1e34a2d7
Vitor Nascimento <vitor.gan21@gmail.com>
2025-02-28 15:46:17 -0300

    test: list genre endpoint

--- infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/api/GenreAPITest.java
+++ infrastructure/src/test/java/com/codeflix/admin/catalogo/infrastructure/api/GenreAPITest.java
@@ -3,8 +3,11 @@ package com.codeflix.admin.catalogo.infrastructure.api;
 import com.codeflix.admin.catalogo.ControllerTest;
 import com.codeflix.admin.catalogo.application.genre.create.CreateGenreOutput;
 import com.codeflix.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import com.codeflix.admin.catalogo.application.genre.delete.DeleteGenreUseCase;
 import com.codeflix.admin.catalogo.application.genre.retrieve.get.GenreOutput;
 import com.codeflix.admin.catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
+import com.codeflix.admin.catalogo.application.genre.retrieve.list.ListGenreOutput;
+import com.codeflix.admin.catalogo.application.genre.retrieve.list.ListGenreUseCase;
 import com.codeflix.admin.catalogo.application.genre.update.UpdateGenreOutput;
 import com.codeflix.admin.catalogo.application.genre.update.UpdateGenreUseCase;
 import com.codeflix.admin.catalogo.domain.category.CategoryID;
@@ -12,6 +15,7 @@ import com.codeflix.admin.catalogo.domain.exceptions.NotFoundException;
 import com.codeflix.admin.catalogo.domain.exceptions.NotificationException;
 import com.codeflix.admin.catalogo.domain.genre.Genre;
 import com.codeflix.admin.catalogo.domain.genre.GenreID;
+import com.codeflix.admin.catalogo.domain.pagination.Pagination;
 import com.codeflix.admin.catalogo.domain.validation.Error;
 import com.codeflix.admin.catalogo.domain.validation.handlers.Notification;
 import com.codeflix.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
@@ -48,6 +52,12 @@ public class GenreAPITest {
 
     @MockBean
     private UpdateGenreUseCase updateGenreUseCase;
+    
+    @MockBean
+    private DeleteGenreUseCase deleteGenreUseCase;
+
+    @MockBean
+    private ListGenreUseCase listGenreUseCase;
 
     @Test
     public void shouldReturnGenreIdWhenCallingCreateGenreGivenAValidCommand() throws Exception {
@@ -236,5 +246,89 @@ public class GenreAPITest {
                         && Objects.equals(expectedIsActive, cmd.active())
                         && Objects.equals(expectedId, cmd.id()))
         );
+    }
+
+    @Test
+    public void shouldBeOkWhenCallingDeleteGenreGivenAValidId() throws Exception {
+        final var expectedId = "123";
+
+        Mockito.doNothing()
+                .when(deleteGenreUseCase)
+                .execute(Mockito.any());
+
+        final var aRequest = MockMvcRequestBuilders.delete("/genres/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON);
+
+        final var aResponse = this.mvc.perform(aRequest);
+
+        aResponse.andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        Mockito.verify(deleteGenreUseCase).execute(Mockito.eq(expectedId));
+    }
+
+    @Test
+    public void shouldBeOkWhenCallingDeleteGenreGivenAnValidId() throws Exception {
+        final var expectedId = "invalid-123";
+
+        Mockito.doNothing()
+                .when(deleteGenreUseCase)
+                .execute(Mockito.any());
+
+        final var aRequest = MockMvcRequestBuilders.delete("/genres/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON);
+
+        final var aResponse = this.mvc.perform(aRequest);
+
+        aResponse.andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        Mockito.verify(deleteGenreUseCase).execute(Mockito.eq(expectedId));
+    }
+
+    @Test
+    public void shouldReturnGenresWhenCallingListGenresGivenValidParams() throws Exception {
+        final Genre aGenre = Genre.createGenre("Action", false);
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "ac";
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+        final var expectedItemsCount = 1;
+        final var expectedTotal = 1;
+
+        final List<ListGenreOutput> expectedItems = List.of(ListGenreOutput.create(aGenre));
+
+        Mockito.when(listGenreUseCase.execute(Mockito.any()))
+                .thenReturn(new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems));
+
+        final var aRequest = MockMvcRequestBuilders.get("/genres")
+                .queryParam("page", String.valueOf(expectedPage))
+                .queryParam("perPage", String.valueOf(expectedPerPage))
+                .queryParam("sort", expectedSort)
+                .queryParam("dir", expectedDirection)
+                .queryParam("search", expectedTerms)
+                .accept(MediaType.APPLICATION_JSON);
+
+        final var aResponse = this.mvc.perform(aRequest);
+
+        aResponse.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(expectedPage)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(expectedPerPage)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(expectedTotal)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(expectedItemsCount)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].id", Matchers.equalTo(aGenre.getId().getValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo(aGenre.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].is_active", Matchers.equalTo(aGenre.isActive())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].created_at", Matchers.equalTo(aGenre.getCreatedAt().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].deleted_at", Matchers.equalTo(aGenre.getDeletedAt().toString())));
+
+        Mockito.verify(listGenreUseCase).execute(Mockito.argThat(args ->
+                Objects.equals(expectedPage, args.page())
+                && Objects.equals(expectedPerPage, args.perPage())
+                && Objects.equals(expectedDirection, args.direction())
+                && Objects.equals(expectedSort, args.sort())
+                && Objects.equals(expectedTerms, args.terms())
+        ));
+
     }
 }
