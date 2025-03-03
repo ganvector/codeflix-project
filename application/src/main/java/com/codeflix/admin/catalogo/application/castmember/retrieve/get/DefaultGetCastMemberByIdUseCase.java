9b046ac75fd8d9a381c12006c8eeebb2037ddb3e
Vitor Nascimento <vitor.gan21@gmail.com>
2025-03-03 18:49:12 -0300

    feat: add get cast member by id use case

---
+++ application/src/main/java/com/codeflix/admin/catalogo/application/castmember/retrieve/get/DefaultGetCastMemberByIdUseCase.java
@@ -0,0 +1,25 @@
+package com.codeflix.admin.catalogo.application.castmember.retrieve.get;
+
+import com.codeflix.admin.catalogo.domain.castmember.CastMember;
+import com.codeflix.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.codeflix.admin.catalogo.domain.castmember.CastMemberID;
+import com.codeflix.admin.catalogo.domain.exceptions.NotFoundException;
+
+import java.util.Objects;
+
+public final class DefaultGetCastMemberByIdUseCase extends GetCastMemberByIdUseCase {
+
+    private final CastMemberGateway castMemberGateway;
+
+    public DefaultGetCastMemberByIdUseCase(final CastMemberGateway castMemberGateway) {
+        this.castMemberGateway = Objects.requireNonNull(castMemberGateway);
+    }
+
+    @Override
+    public CastMemberOutput execute(final String anIn) {
+        final var aMemberId = CastMemberID.load(anIn);
+        return this.castMemberGateway.findById(aMemberId)
+                .map(CastMemberOutput::from)
+                .orElseThrow(() -> NotFoundException.raise(CastMember.class, aMemberId));
+    }
+}
