7cab1495c44a910a296343bb92ba44dc93f32cff
Vitor Nascimento <vitor.gan21@gmail.com>
2025-03-04 12:31:51 -0300

    feat: add gateway to cast member

--- infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
@@ -1,6 +1,7 @@
 package com.codeflix.admin.catalogo.infrastructure.castmember.persistence;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
